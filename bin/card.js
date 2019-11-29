70439455bf016228fbb56bbcdb3a8df2991eb2aa
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-29 13:23:46 -0300

    feat: businesscard

--- bin/card.js
+++ bin/card.js
@@ -1,9 +1,62 @@
 #!/usr/bin/env node
 // 👆 Used to tell Node.js that this is a CLI tool
 
-'use strict'
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
 
-const fs = require('fs')
-const path = require('path')
-const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
-console.log(output)
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Nicolas Gebauer /'),
+  handle: chalk.cyan('negebauer'),
+  work: chalk.white('Software Engineer at Metabolic'),
+  twitter: chalk.cyan('https://twitter.com/negebauer'),
+  github: chalk.cyan('https://github.com/negebauer'),
+  linkedin: chalk.cyan('https://linkedin.com/in/negebauer'),
+  web: chalk.cyan('https://negebauer.com'),
+  npx: chalk.white('npx negebauer'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:'),
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding
+
+// eslint-disable-next-line no-console
+console.log(chalk.green(boxen(output, options)))
